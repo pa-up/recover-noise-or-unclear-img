16499a783498e04289c005d2855a0e3cc04ed12b
xxxx <xxxxxx@gmail.com>
2023-03-29 09:53:44 +0900

    [Add] index

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import streamlit as st
-import convert_unclear_img
+import views
 
-st.title('モザイク・ノイズ画像の復元サイト')
+st.title('モザイク・ノイズ画像の復元')
 
-col1, col2 = st.columns(2)
-
-if col1.button('虫食い・モザイク画像の復元はこちら'):
-    st.write("\n")
-    # ボタンが押されたときに実行する関数
+def change_page(page):
+    st.session_state["page"] = page
+    
+def mosaic_page():
+    # 別ページへの遷移
+    st.button("ノイズ画像の復元はこちら >", on_click=change_page, args=["page2"])
+    st.write('\n')
+    
+    # 画像処理の実行
     model_path = "tf_CAE_ImgNet_mosaic.h5"
-    convert_unclear_img.display(model_path)
-    st.write("\n")
-    st.write("\n")
+    page_subtitle = "<h3>虫食い・モザイク画像の復元</h3>"
+    views.upload_and_display_img(model_path, page_subtitle)
 
-if col2.button('ノイズが多い画像の復元はこちら'):
-    st.write("\n")
-    # ボタンが押されたときに実行する関数
+def noise_page():
+    # 別ページへの遷移
+    st.button("虫食い・モザイク画像の復元はこちら >", on_click=change_page, args=["page1"])
+    st.write('\n')
+
+    # 画像処理の実行
     model_path = "tf_CAE_ImgNet_noise.h5"
-    convert_unclear_img.display(model_path)
-    st.write("\n")
+    page_subtitle = "<h3>ノイズ画像の復元</h3>"
+    views.upload_and_display_img(model_path, page_subtitle)
+
+
+# メイン
+def main():
+    # セッション状態を取得
+    session_state = st.session_state
+
+    # セッション状態によってページを表示
+    if "page" not in session_state:
+        session_state["page"] = "page1"
+
+    if session_state["page"] == "page1":
+        mosaic_page()
+    elif session_state["page"] == "page2":
+        noise_page()
+
+if __name__ == "__main__":
+    main()
--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import convert_img
-import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
@@ -8,36 +7,31 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
+def run_CAE(img , model):
+    # 入力画像をモデルの入力サイズにリサイズ
+    resized_length = 200
+    resized_square_img , resized_input_img = convert_img.resize_to_square(img , resized_length)
+    img = np.reshape(resized_square_img, (1, resized_length, resized_length, 3))
+    # 入力画像の正規化
+    model_input = img / 255.
+    # 予測結果を出力
+    pred_result = model.predict(model_input)
+    # 予測結果を画像に変換
+    pred_img = pred_result * 255
+    output_img = pred_img[0][ : int(resized_input_img.shape[0] * 0.98) , : int(resized_input_img.shape[1] * 0.98) ]
+    return output_img , resized_input_img
 
-def display(model_path):
-    st.write("<h3>虫食い・モザイク画像の復元</h3>", unsafe_allow_html=True)
 
+def upload_and_display_img(model_path, page_subtitle):
+    st.write(page_subtitle, unsafe_allow_html=True)
     # 学習済みのCAEモデルを読み込む
     tf_CAE_model = load_model(model_path)
 
     # 画像のアップロード
     uploaded_file = st.file_uploader("画像ファイルをアップロードしてください", type=['jpg', 'jpeg', 'png'])
-    print(f"アップロード直後")
-    print(f"uploaded_file : {uploaded_file}")
-
-
-    def convert_img_func(img , model):
-        # 入力画像をモデルの入力サイズにリサイズ
-        resized_length = 200
-        resized_square_img , resized_input_img = convert_img.resize_to_square(img , resized_length)
-        img = np.reshape(resized_square_img, (1, resized_length, resized_length, 3))
-        # 入力画像の正規化
-        model_input = img / 255.
-        # 予測結果を出力
-        pred_result = model.predict(model_input)
-        # 予測結果を画像に変換
-        pred_img = pred_result * 255
-        output_img = pred_img[0][ : int(resized_input_img.shape[0] * 0.98) , : int(resized_input_img.shape[1] * 0.98) ]
-        return output_img , resized_input_img
 
 
     if uploaded_file is not None:
-        print(f"アップロードは成功")
         # PILで画像を開く
         img_pil = Image.open(uploaded_file)
         
@@ -47,7 +41,7 @@ def display(model_path):
         img_np = img_np[..., :3]
         
         # アップロード画像に画像認識を実行
-        output_img , resized_input_img = convert_img_func(img_np , tf_CAE_model)
+        output_img , resized_input_img = run_CAE(img_np , tf_CAE_model)
         
         # PILのImageに戻す
         output_pil = Image.fromarray(np.uint8(output_img))
@@ -58,3 +52,6 @@ def display(model_path):
         col1.image(input_pil, caption='元の画像', use_column_width=True)
         # 処理結果を表示
         col2.image(output_pil, caption='処理結果', use_column_width=True)
+
+if __name__ == "__main__":
+    upload_and_display_img()
